36e34ad0916323c33e95c85501415e87f9e6d4ff
bipin-dev <bipps.dev@gmail.com>
2020-09-06 13:35:06 +0530

    change server ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: "http://ec2-18-217-247-175.us-east-2.compute.amazonaws.com/api"
+  server: "http://ec2-3-21-56-236.us-east-2.compute.amazonaws.com/api",
 };
